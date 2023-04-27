36aafaf363e4354f8d62013e91370333b084fe0d
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-04-27 10:56:36 +0330

    added contactDetail styles

--- src/components/ContactDetail/ContactDetail.jsx
+++ src/components/ContactDetail/ContactDetail.jsx
@@ -1,5 +1,37 @@
+import userImage from "../../assets/userImage.jfif";
+import "./ContactDetail.css";
+import { Link } from "react-router-dom";
+
 const ContactDetail = () => {
-  return <div>ContactDetail</div>;
+  return (
+    <div className="contactDetail">
+      <div className="title">
+        <h2>Contact detail</h2>
+      </div>
+      <div className="box">
+        <img src={userImage} alt="user" />
+      </div>
+      <div className="box">
+        <p>Contact name : Abolfazl</p>
+      </div>
+      <div className="box">
+        <p>Contact Phone: 09108665521</p>
+      </div>
+      <div className="box">
+        <p>Contact description : this is description</p>
+      </div>
+      <div className="box">
+        <Link to="/edit">
+          <button type="button" className="btn editBtn">
+            Edit this Contact
+          </button>
+        </Link>
+        <button type="button" className="btn deleteBtn">
+          Delete this Contact
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ContactDetail;
